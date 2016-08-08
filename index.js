45628d537181e401cd4a1f8a8969b60c1eb03df4
zxcpoiu <zxcpoiu@gmail.com>
2016-08-08 14:03:21 +0800

    add flow type check

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ var { Platform, NativeModules } = require('react-native');
 const _FirebaseCrashReport = NativeModules.RNFirebaseCrashReport;
 
 export default class FirebaseCrashReport {
-    static log(message) {
+
+    static log(message: string) {
         _FirebaseCrashReport.log(message);
     }
 
-    static logcat(message, level = null, tag = null) {
+    static logcat(message: string, level: number = null, tag: string = null) {
         if (Platform.OS === 'ios') {
             _FirebaseCrashReport.logcat(message);
         } else if (Platform.OS === 'android') {
-            if (level === null) {
-                // set level to DEBUG
-                level = 3;
-            }
-
-            if (tag === null) {
-                tag = 'RNFirebaseCrashReport'
-            }
-
+            level = (typeof level !== 'number') ? 3 : level; // --- default level: 3 - DEBUG
+            tag = (typeof tag !== 'string') ? 'RNFirebaseCrashReport' : tag;
             _FirebaseCrashReport.logcat(level, tag, message);
         }
     }
